6b6513c949f470b34ee239ac8ddd4154f4db8c2d
Ajay Chauhan <exactlyajay.chauhan@gmail.com>
2024-10-30 14:47:38 +0530

    main: add flag definitions and main execution loop

--- src/main.c
+++ src/main.c
@@ -25,6 +25,16 @@ enum
 uint16_t reg[R_COUNT]; // store the registers in an array
 
 // why we use enum--> automatic incrementing, Sequential Representation
+enum
+{
+    FL_POS = 1 << 0, /* FL_POS is set to 1 shifted left by 0 bits.
+                         In binary (16-bit): 0000 0000 0000 0001
+                         Decimal value: 1
+                         Represents the "positive" state flag. */
+    FL_ZRO = 1 << 1, /* Z */
+    FL_NEG = 1 << 2, /* N */
+};
+
 
 /*Instruction Set*/
 enum
@@ -46,3 +56,82 @@ enum
     OP_LEA,    /* load effective address */
     OP_TRAP    /* execute trap */
 };
+
+int main(int argc, const char* argv[])
+{
+    
+    reg[R_COND] = FL_ZRO;
+
+    /* Set the PC to the starting memory address.
+       0x3000 is typically the default start address in this context. */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        /* Read the instruction at the address in the PC register,
+           then increment the PC to point to the next instruction. */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        
+        /* DECODE */
+        /* The opcode is determined by shifting the instruction 12 bits to the right.
+           The opcode is stored in the upper 4 bits of the 16-bit instruction. */
+        uint16_t op = instr >> 12;
+
+        /* EXECUTE */
+        switch (op)
+        {
+            case OP_ADD:
+                /* @{ADD} */
+                break;
+            case OP_AND:
+                /* @{AND} */
+                break;
+            case OP_NOT:
+                /* @{NOT} */
+                break;
+            case OP_BR:
+                /* @{BR} */
+                break;
+            case OP_JMP:
+                /* @{JMP} */
+                break;
+            case OP_JSR:
+                /* @{JSR} */
+                break;
+            case OP_LD:
+                /* @{LD} */
+                break;
+            case OP_LDI:
+                /* @{LDI} */
+                break;
+            case OP_LDR:
+                /* @{LDR} */
+                break;
+            case OP_LEA:
+                /* @{LEA} */
+                break;
+            case OP_ST:
+                /* @{ST}. */
+                break;
+            case OP_STI:
+                /* @{STI} */
+                break;
+            case OP_STR:
+                /* @{STR} */
+                break;
+            case OP_TRAP:
+                /* @{TRAP} */
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                /* @{BAD OPCODE} */
+                break;
+        }
+    }
+    
+    /* @{Shutdown} */
+}
